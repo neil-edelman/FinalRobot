ff9d89c8fc948b57e73cd0c7cf49a23a0f520857
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-10-30 20:26:39 -0400

    added Swagbot

---
+++ Code/src/Swagbot.java
@@ -0,0 +1,39 @@
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
+public class Swagbot extends Robot {
+
+	Colour       colour = new Colour();
+
+	public Swagbot() {
+		super();
+	}
+
+	/** this is used to rotate the robot at a constant speed */
+	public void turnConstantlyTo(final float angleTo,final int speed) {
+		status = Status.LOCALISING;
+		this.setSpeeds(-speed,speed);
+	}
+
+//	void localise(boolean doLight) {
+//
+//      status = Status.LOCALISING;
+//      
+//      USLocalizer usl = new USLocalizer(this, new UltrasonicSensor(SensorPort.S4), USLocalizer.LocalizationType.FALLING_EDGE);
+//      usl.doLocalization();
+//      if(doLight) {
+//         robot.travelTo(-3f,-3f);
+//         robot.turnTo(45f);
+//         LightLocalizer lsl = new LightLocalizer(this, new LightSensor(SensorPort.S1));
+//         lsl.doLocalization();
+//      }
+//
+//		status = Status.IDLE;
+//	}
+
+	public Colour.Value getColour() {
+		return colour.getColourValue();
+	}
+
+}
