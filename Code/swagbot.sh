565de69f31f90bce00867184c2dcfa67585565b8
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-11-18 18:20:53 -0500

    added Swagbot upload

---
+++ Code/swagbot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd bin
+#nxj -b -n "Sex Robot" Driver
+#nxj -b -d 0016530FE494 AlexDriver
+# Swagbot
+nxj -b -d 0016530E5586 AlexDriver
+
+# to upload:
+# turn the robot on
+# turn bluetooth on
+# run upload.sh, wait 30s
+
+# to rconsole:
+# run the programme on the robot
+# run nxjconsoleviewer &
+# type "Sex Robot" or "0016530FE494," connect
